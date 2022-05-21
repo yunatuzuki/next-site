f449a202e88d1ee34284a1cd6c73a75004b4df91
yunatuzuki <yuzunacchan@gmail.com>
2022-05-21 14:20:27 +0900

    SWRエラー修正

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import Link from "next/link"
-import useSWR from 'SWR'
+import useSWR from 'swr'
 
 import Header from './components/header'
 import Content from './components/content'
